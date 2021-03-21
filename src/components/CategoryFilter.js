4267a963b8723118f3a516e660f8b45987ea6c77
Sam Ferebee <sam.ferebee@gmail.com>
2021-03-21 16:18:11 -0400

    commit tree 733bc852e8bfbfdcc1b63d9e03f50dc2a5606bc5

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+import Button from "./Button"
 
-function CategoryFilter() {
+function CategoryFilter({allCategories, filterCallback}) {
+  const [selected,setSelected]= useState("")
+
+  const handleSelected = (id) => {
+    setSelected(id);
+  }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {allCategories.map((cat)=>
+        <Button 
+          key={cat} 
+          category={cat} 
+          filterCallback={filterCallback} 
+          setSelected={handleSelected} 
+          cn={cat === selected? "selected": ""} 
+        /> 
+      )}
     </div>
   );
 }
